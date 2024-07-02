95d8391f4df2729e665f44c9dac920fded3ede62
Wayu Ragwongsiri <windywayu2000@gmail.com>
2024-07-02 09:17:24 +0700

    fix connectorx

--- dags/test-dag.py
+++ dags/test-dag.py
@@ -3,6 +3,10 @@ from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 
+import polars as pl
+import subprocess
+import logging
+
 @dag(
     dag_id='test-dag',
     start_date=days_ago(1),
@@ -13,9 +17,18 @@ from airflow.operators.empty import EmptyOperator
 def test_dag():
     upstream = EmptyOperator(task_id="start")
 
+    @task(task_id="test")
+    def test():
+        uri = "postgresql://wayu:xTUu28E5@10.121.101.144:5432/dvdrental"
+        query = "SELECT * FROM actor"
+        try:
+            df = pl.read_database_uri(query, uri, engine="connectorx")
+        except:
+            df = pl.read_database_uri(query, uri, engine="adbc")
+
     downstream = EmptyOperator(task_id="end")
 
-    upstream >> downstream
+    upstream >> test() >> downstream
 
 test_dag()
 
